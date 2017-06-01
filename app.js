f200f5ca532b6b9565e925d7c340a57b0e2fd8b4
zagaynova <masha.zagaynova@gmail.com>
2017-06-01 20:47:00 +0200

    adding messages working

--- app.js
+++ app.js
@@ -1,38 +1,63 @@
 var pg = require('pg');
-var express = require( 'express')
+var express = require( 'express');
+var bodyParser = require('body-parser');
 const pug = require('pug');
-// const router = express.Router();
-// const path = require('path');
 
-var go = 000000060;
 const app = express()
-
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/bulletinboard';
 
-console.log(connectionString);
+function addMessage (title, body) {
+	pg.connect(connectionString, function(err, client, done) {
+		var insertQuery = "insert into messages (title, body) values ('" + title + "', '" + body + "')";
+	  	client.query(insertQuery, function(err, result) {
+		    done();
+		    pg.end();
+	  	});
+	});
+}
+
+function deleteMessage () {
+	pg.connect(connectionString, function(err, client, done) {
+		var deleteQuery;
+		//deleteQuery = "delete from messages where title = 'iets'";
+		client.query(deleteQuery, function(err, result) {
+	    	done();
+	    	pg.end();
+	  	});
+	});
+}
+
 
 const allPosts = pug.compileFile('./views/show.pug');
+const writePosts = pug.compileFile('./views/write.pug');
+
 //making web server
 app.get('/', (req, res) => { 
 //connect to a database
 	pg.connect(connectionString, function(err, client, done) {
-
-		//console.log(err);
-		//console.log(client);
-
-	  client.query('select * from messages', function(err, result) {
-
-
-	    //console.log(result.rows[0].body);
-	    done();
-	    pg.end();
-	    res.send(allPosts({"messages" : result.rows}));
-	  });
+	  	client.query('select * from messages', function(err, result) {
+		    done();
+		    pg.end();
+		    res.send(allPosts({"messages" : result.rows}));
+		});
 	});
 })
 
 //write router
+app.get('/newmessage',(req, res) => {
+	res.send(writePosts());
+
+});
+
+
+app.post('/addmessage', function(req, res){
+	console.log(req.body) 
+	addMessage(req.body.title, req.body.body);
+	res.redirect('/')
+})
+
 
 const listener = app.listen(80, () => {
     console.log('server has started at ', listener.address().port)
